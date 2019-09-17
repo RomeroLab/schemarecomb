c748d881021bde9fc76dd2b3fee0614169fa2bbc
Bennett J. Bremer <bbremer@wisc.edu>
2019-09-17 14:16:51 -0500

    updated step1

--- Step1/generate_alignments.py
+++ Step1/generate_alignments.py
@@ -1,55 +1,70 @@
 import os
-from subprocess import Popen
 
 from Bio import Seq, SeqRecord, SeqIO
 
 import tools
 
-in_fn = 'sequences.fasta'        # name of sequences file read by muscle
+in_fn = 'test_seqs.fasta'        # name of sequences file read by muscle
 out_file_prefix = 'alignment'    # prefix of output files
-pdb_fn = '1GNX.pdb'
+pdb_fn = 'test.pdb'
 
-# Align sequences with muscle
-Popen(f'muscle -in {in_fn} -out {out_file_prefix}_AA.fasta',
-      shell=True).wait()
+tools.run_muscle(in_fn, out_file_prefix + '_AA.fasta')
 
+# Load muscle input fasta file
 alignment_SRs = list(SeqIO.parse(f'{out_file_prefix}_AA.fasta', 'fasta'))
 names = [str(i.id) for i in alignment_SRs]
 descriptions = [str(i.description) for i in alignment_SRs]
 AA_seqs = [str(i.seq) for i in alignment_SRs]
 AA_alignment = list(zip(*AA_seqs))  # get list of AAs at each position
 
+# Load sequence from pdb and temporarily add to AA alignment
 pdb_AAs = tools.read_PDB(pdb_fn)
-pdb_Seq = Seq.Seq(tools.get_PDB_seq(pdb_AAs))
+pdb_Seq = tools.get_PDB_seq(pdb_AAs)
+pdb_Seq = Seq.Seq(pdb_Seq)
 pdb_SeqRecord = SeqRecord.SeqRecord(pdb_Seq, name='PDB',
                                     description=f'{pdb_fn} sequence')
 temp_SRs = [pdb_SeqRecord] + alignment_SRs
-SeqIO.write(temp_SRs, 'temp_seqs.fasta', 'fasta')
-Popen(f'muscle -in temp_seqs.fasta -out PDB_align.fasta', shell=True).wait()
-os.delete('temp_seqs.fasta')
-pdb_align_SRs = SeqIO.read('PDB_align.fasta', 'fasta')
-pdb_align_seqs = [str(i.seq) for i in pdb_align_SRs]
-pdb_alignment = list(zip(*AA_seqs))
 
-pdb_residues = iter(pdb_AAs)
-pos = 0
-for pdb_AA, parent_AAs in pdb_alignment:
-    valid_parent_pos = all([a == '-' for a in parent_AAs])
+# Align pdb sequence to AA aligned sequences
+SeqIO.write(temp_SRs, 'temp_seqs.fasta', 'fasta')
+tools.run_muscle('temp_seqs.fasta', 'PDB_align.fasta')
+os.remove('temp_seqs.fasta')
+pdb_align_SRs = SeqIO.parse('PDB_align.fasta', 'fasta')
+pdb_align_seqs = [str(i.seq) for i in pdb_align_SRs]
+pdb_alignment = list(zip(*pdb_align_seqs))
+
+# Renumber pdb residues and save new pdb
+pdb_AAs_residue = iter(pdb_AAs)
+pos = 0    # PDB will be 1-indexed for consistency with other PDB files
+for pdb_AA, *parent_AAs in pdb_alignment:
+    valid_parent_pos = any([a != '-' for a in parent_AAs])
     if valid_parent_pos:
         pos += 1
     if pdb_AA != '-':
-        AA = next(pdb_residues)
+        AA = next(pdb_AAs_residue)
         assert tools.seq1(AA.resName) == pdb_AA
         if valid_parent_pos:
             AA.resSeq = pos
         else:
             AA.resSeq = None
+tools.write_PDB(pdb_AAs, 'renumbered_' + pdb_fn)
+
+# Calculate contacts
+pdb_AAs = list(tools.read_PDB('renumbered_' + pdb_fn))
+for AA in pdb_AAs:
+    AA.resSeq -= 1   # make index start from 0
+contacts = []
+for index, AA1 in enumerate(pdb_AAs):
+    for AA2 in pdb_AAs[index+1:]:
+        if AA1.d(AA2) < 4.5:
+            contacts.append((AA1.resSeq, AA2.resSeq))
 
 # Add stop codons if necessary
 if AA_alignment[-1] != tuple(['*'] * len(alignment_SRs)):
     AA_alignment.append(tuple(['*'] * len(alignment_SRs)))
 
-
+# Make and save codon alignments
 CDN_align = [tools.get_pos_CDNs(pos) for pos in AA_alignment]
 CDN_seqs = [''.join(CDN_pos) for CDN_pos in zip(*CDN_align)]
-# save_SeqRecords(CDN_seqs, f'{out_file_prefix}_CDN.fasta')
+tools.save_SeqRecords(CDN_seqs, names, descriptions, 
+                      f'{out_file_prefix}_CDN.fasta')
--- Step1/tools.py
+++ Step1/tools.py
@@ -1,4 +1,6 @@
 import random
+from subprocess import Popen
+import sys
 
 from Bio import Seq, SeqRecord, SeqIO
 from Bio.SeqUtils import seq1
@@ -15,12 +17,17 @@ AA_C31 = {'A': ('GCT', 'GCA'), 'R': ('CGT', 'CGA'), 'N': ('AAT',),
 
 class atom(object):
     def __init__(self, line):
+        self.line = line
         self.resName = line[17:20].strip()
         self.resSeq = int(line[22:26].strip())
         self.x = float(line[30:38].strip())
         self.y = float(line[38:46].strip())
         self.z = float(line[46:54].strip())
 
+    def __str__(self):
+        line = list(self.line)
+        line[22:26] = list('{:4d}'.format(self.resSeq))
+        return ''.join(line)
 
 def d(a1, a2):
     return ((a1.x - a2.x)**2 + (a1.y - a2.y)**2 + (a1.z - a2.z)**2) ** (1/2)
@@ -49,10 +56,25 @@ class residue(object):
         return self._resSeq
 
     @resSeq.setter
-    def resSeq(self, resSeq):
+    def resSeq(self, new_resSeq):
         for a in self.atoms:
-            a.resSeq = resSeq
-        self.resSeq = resSeq
+            a.resSeq = new_resSeq
+        self._resSeq = new_resSeq
+
+    def __repr__(self):
+        return f'residue: {self.resName} {self.resSeq}'
+
+
+def run_muscle(in_fn, out_fn):
+    # Align sequences with muscle
+    print(f'Running muscle with {in_fn}, saving to {out_fn}.', flush=True) 
+    Popen(f'muscle -in {in_fn} -out {out_fn}',
+          shell=True).wait()
+    out_SRs = {SR.name: SR for SR in SeqIO.parse(out_fn, 'fasta')}
+    in_labels = [SR.name for SR in SeqIO.parse(in_fn, 'fasta')]
+    stable_out_SRs = [out_SRs[label] for label in in_labels]
+    SeqIO.write(stable_out_SRs, out_fn, 'fasta')
+    print('\n\n Muscle done.')
 
 
 def read_PDB(pdb_fn):
@@ -67,8 +89,16 @@ def read_PDB(pdb_fn):
     return AAs.values()
 
 
+def write_PDB(residues, fn):
+    with open(fn, 'w') as fn:
+        for AA in residues:
+            if AA.resSeq != None:
+                for atom in AA.atoms:
+                    fn.write(str(atom) + '\n')
+
+
 def get_PDB_seq(AAs):
-    return [seq1(AA.resName) for AA in AAs]
+    return ''.join([seq1(AA.resName) for AA in AAs])
 
 
 def save_SeqRecords(seqs, names, descriptions, handle):

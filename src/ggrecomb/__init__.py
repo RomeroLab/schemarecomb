b5edf921af742cd953d406f22402ff5eb083f1b4
Bennett J. Bremer <bennettjamesbremer@gmail.com>
2021-09-01 15:43:08 -0400

    Add back module as ggrecomb submodule access.

--- src/ggrecomb/__init__.py
+++ src/ggrecomb/__init__.py
@@ -1,7 +1,12 @@
 from ggrecomb.pdb_structure import _PDBStructure as PDBStructure
 from ggrecomb.parent_alignment import _ParentSequences as ParentSequences
 
+from . import energy_functions
+
 __all__ = [
     'ParentSequences',
     'PDBStructure',
+    'pdb_structure',
+    'parent_alignment',
+    'energy_functions',
 ]

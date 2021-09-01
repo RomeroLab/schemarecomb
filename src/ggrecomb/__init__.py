3d91f191b1fd190750ac73df4c5a073a8f305a55
Bennett J. Bremer <bennettjamesbremer@gmail.com>
2021-09-01 15:36:47 -0400

    Remove module as ggrecomb submodule access.

--- src/ggrecomb/__init__.py
+++ src/ggrecomb/__init__.py
@@ -1,14 +1,7 @@
 from ggrecomb.pdb_structure import _PDBStructure as PDBStructure
 from ggrecomb.parent_alignment import _ParentSequences as ParentSequences
 
-from ggrecomb import parent_alignment
-from ggrecomb import pdb_structure
-from ggrecomb import energy_functions
-
 __all__ = [
     'ParentSequences',
     'PDBStructure',
-    'energy_functions',
-    'parent_alignment',
-    'pdb_structure',
 ]
